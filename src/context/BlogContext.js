c651754d105e7101623c93a008dfdf0932d818f7
Anirup Patnaik <anirup049@gmail.com>
2020-06-17 03:37:52 +0530

    Rendering a list of posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,14 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-    return <BlogContext.Provider value={5}>
+    const blogPost = [
+        { title: "Blog Post #1" },
+        { title: "Blog Post #2" },
+        { title: "Blog Post #3" }
+    ];
+
+
+    return <BlogContext.Provider value={blogPost}>
         {children}
     </BlogContext.Provider>
 };
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,13 +1,20 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-    const value = useContext(BlogContext);
+    const blogPost = useContext(BlogContext);
     return (
         <View>
             <Text>Index Screen</Text>
-            <Text>Value is: {value}</Text>
+            <FlatList 
+                data={blogPost} 
+                keyExtractor={(blogPost) =>  blogPost.title}
+                renderItem={({ item }) => {
+                    return (
+                        <Text>{item.title}</Text>
+                    )
+                }} />
         </View>
     )
 };
